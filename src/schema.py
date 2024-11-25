fb26a1e8e5a1963c6b9bad345ca12cf1093718d8
Kim Wooseok <eypk9673@gendata.kr>
2024-11-25 18:19:12 +0900

    1125 feat: make generic response model

--- src/schema.py
+++ src/schema.py
@@ -1,3 +1,21 @@
+from pydantic import BaseModel
+from pydantic.generics import GenericModel
 from sqlalchemy.orm import declarative_base
+from typing import Generic, TypeVar, List, Optional
 
-Base = declarative_base()
+TableBase = declarative_base()
+
+
+T = TypeVar("T")
+
+
+class MetaData(BaseModel):
+    itemsPerPage: Optional[int] = None
+    totalItems: Optional[int] = None
+    pageIndex: Optional[int] = None
+    totalPages: Optional[int] = None
+
+
+class GenericResponse(GenericModel, Generic[T]):
+    data: Optional[MetaData]
+    items: List[T]
