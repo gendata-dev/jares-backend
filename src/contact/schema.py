c6f7efd950366e5316afd158730fe055422101e0
Kim Wooseok <eypk9673@gendata.kr>
2024-12-04 14:16:34 +0900

    1204 feat: use composite key for join table

--- src/contact/schema.py
+++ src/contact/schema.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Text
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship
 
 from src.schema import TableBase
@@ -51,22 +51,28 @@ class Equipment(TableBase):
 
 
 class ContactCrop(TableBase):
+	"""관계 테이블"""
 	__tablename__ = "contact_crops"
 
-	id = Column(Integer, primary_key=True, autoincrement=True)
 	contact_id = Column(Integer, ForeignKey("contacts.id"), nullable=False)
 	crop_id = Column(Integer, ForeignKey("crops.id"), nullable=False)
 
 	contacts = relationship("Contact", back_populates="contact_crops")
 	crops = relationship("Crop", back_populates="contact_crops")
 
+	__table_args__ = tuple(PrimaryKeyConstraint(contact_id, crop_id))
+
 
 class ContactEquipment(TableBase):
+	"""관계 테이블"""
 	__tablename__ = "contact_equipments"
 
-	id = Column(Integer, primary_key=True, autoincrement=True)
 	contact_id = Column(Integer, ForeignKey("contacts.id"), nullable=False)
 	equipment_id = Column(Integer, ForeignKey("equipments.id"), nullable=False)
 
 	contacts = relationship("Contact", back_populates="contact_equipments")
 	equipments = relationship("Equipment", back_populates="contact_equipments")
+
+	__table_args__ = tuple(PrimaryKeyConstraint(contact_id, equipment_id))
+
+
