353f229e536b3fa58dc981e53db8116151c6bd7c
Kim Wooseok <eypk9673@gendata.kr>
2024-12-09 09:28:45 +0900

    1209 feat: contact, group 도메인 분리

---
+++ src/group/service.py
@@ -0,0 +1,79 @@
+from sqlalchemy import select, insert, update as sa_update, delete as sa_delete
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+
+from src.config import PAGESIZE
+from src.schema import PrimaryKey
+from src.contact.schema import Contact
+
+from .schema import Group, GroupCreate, GroupList
+
+
+def get(*, db_session: Session, group_id: PrimaryKey) -> dict:
+    """Gets a group by its id"""
+    statement = select(Group.id, Group.group_name).where(Group.id == group_id)
+    result = db_session.execute(statement).mappings().first()
+
+    return result
+
+
+def get_all(*, db_session: Session) -> list[dict]:
+    """Returns all groups"""
+    statement = select(Group.id, Group.group_name).order_by(Group.id.desc())
+    result = db_session.execute(statement).mappings().fetchall()
+
+    return result
+
+
+def get_many(*, db_session: Session, page: int) -> list[dict]:
+    """Gets a paginated list of groups"""
+    offset = (page - 1) * PAGESIZE
+    statement = (
+        select(Group.id, Group.group_name)
+        .order_by(Group.id.desc())
+        .limit(PAGESIZE)
+        .offset(offset)
+    )
+    result = db_session.execute(statement).mappings().fetchall()
+
+    return result
+
+
+def create(*, db_session: Session, group_in: GroupCreate) -> dict:
+    """Creates a new group"""
+    statement = (
+        insert(Group)
+        .values(group_name=group_in.groupName)
+        .returning(Group.id, Group.group_name)
+    )
+    result = db_session.execute(statement).mappings().first()
+    db_session.commit()
+
+    return result
+
+
+def update(*, db_session: Session, group_id: PrimaryKey, group_in: GroupCreate) -> dict:
+    """Creates a new group"""
+    statement = (
+        sa_update(Group)
+        .where(Group.id == group_id)
+        .values(group_name=group_in.groupName)
+        .returning(Group.id, Group.group_name)
+    )
+    result = db_session.execute(statement).mappings().first()
+    db_session.commit()
+
+    return result
+
+
+def delete(*, db_session: Session, group_in: GroupList) -> list[dict]:
+    """Deletes multiple existing entity"""
+    statement = (
+        sa_delete(Group)
+        .where(Group.id.in_(group_in.groupIds))
+        .returning(Group.id, Group.group_name)
+    )
+    result = db_session.execute(statement).mappings().fetchall()
+    db_session.commit()
+
+    return result
--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from contextlib import asynccontextmanager
 
 from src.call.router import router as call_router
 from src.auth.router import router as auth_router
+from src.group.router import router as group_router
 from src.survey.router import router as survey_router
 from src.contact.router import router as contact_router
 from src.routine.router import router as routine_router
@@ -18,6 +19,7 @@ app = FastAPI()
 """ 라우터 추가 """
 app.include_router(call_router, prefix="/v1", tags=["call"])
 app.include_router(auth_router, prefix="/user-management", tags=["auth"])
+app.include_router(group_router, prefix="/contact-management", tags=["group"])
 app.include_router(survey_router, prefix="/survey-management", tags=["survey"])
 app.include_router(contact_router, prefix="/contact-management", tags=["contact"])
 app.include_router(routine_router, prefix="/routine-management", tags=["routine"])
